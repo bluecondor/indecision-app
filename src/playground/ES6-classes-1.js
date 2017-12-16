9e1e2513d837a70fc84b255f61913ad977756e52
Petru Conduraru <petruconduraru@outlook.com>
2017-12-16 21:57:55 +0200

    Before asignment to make a subclass

---
+++ src/playground/ES6-classes-1.js
@@ -0,0 +1,38 @@
+class Person {
+  constructor(name = 'Anonymous', age = 0) {
+    this.name = name;
+    this.age = age;
+  }
+  getGreeting() {
+    return `Hi. I'm ${ this.name }!`;
+  }
+  getDescription() {
+    return `${ this.name } is ${ this.age } year(s) old.`
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, major) {
+    super(name, age);
+    this.major = major;
+  }
+  hasMajor() {
+    return !!this.major;
+  }
+  getDescription() {
+    let description = super.getDescription();
+    if (this.hasMajor()) {
+      return description += ` Their major is ${this.major}.`
+    }
+  }
+}
+
+class Traveler extends Person {
+  constructor
+}
+
+const me = new Student('Petru Conduraru', 33, 'Civil Engineering');
+console.log(me.getDescription());
+
+const other = new Student();
+console.log(other.getDescription());
