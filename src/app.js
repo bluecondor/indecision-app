318a6b03c4309e0557b1c758d6ebd20551d27a59
Petru Conduraru <petruconduraru@outlook.com>
2017-12-19 17:38:11 +0200

    importing npm modules

--- src/app.js
+++ src/app.js
@@ -1,12 +1,5 @@
-// import './utils.js';
-// import subtract, { square, add } from './utils.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('App.js is running');
-// console.log(square(4));
-// console.log(add(4, 3));
-// console.log(subtract(100, 81));
-
-import isSenior, { isAdult, canDrink } from './person.js'
-console.log(isAdult(17));
-console.log(canDrink(21));
-console.log(isSenior(68));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
