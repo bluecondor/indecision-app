803181793bee1f20d57d1955170e870742756859
Petru Conduraru <petruconduraru@outlook.com>
2017-12-19 19:07:28 +0200

    ES6 class properties

--- src/app.js
+++ src/app.js
@@ -4,3 +4,21 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+class OldSyntax {
+  constructor() {
+    this.name = 'Mike';
+  }
+}
+
+const oldSyntax = new OldSyntax();
+console.log(OldSyntax);
+
+//-----------
+
+class NewSyntax {
+  name = 'Jen';
+}
+
+const newSyntax = new NewSyntax();
+console.log(newSyntax);
